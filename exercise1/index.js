73c09d960349848fb91c214469281aff886b7a0c
daprilianza <aprilianzadendy@gmail.com>
2021-01-22 13:50:26 +0700

    first commit

---
+++ exercise1/index.js
@@ -0,0 +1,53 @@
+/* Soal No.1 */
+let dataDiri = {
+    //nama lengkap//
+    name: "Dendy Aprilianza",
+    //asal daerah//
+    asal: "Jakarta",
+    //tempat dan tanggal lahir//
+    ttl: "Jakarta, 12/04/1999",
+    //usia terkini//
+    umur: "22 tahun",
+    //posisi pada pekerjaan terakhir//
+    posisi: "staff marketing"
+}
+
+console.log (dataDiri)
+console.log (dataDiri.name)
+console.log (dataDiri.asal)
+console.log (dataDiri.ttl)
+console.log (dataDiri.umur)
+console.log (dataDiri.posisi)
+
+/* Soal Interaksi User */
+
+    prompt ("Input Username");
+    prompt ("Input Negara asal");
+    data = confirm ("Apakah data anda sudah benar ?")
+    alert ("Terima kasih sudah mengisi form")
+
+/* Soal Operasi Matematika */
+// 25 + 10 //
+var a = 25;
+var b = 10;
+
+var total1 = a + b;
+console.log (total1)
+
+//100 * 2 //
+var c = 100;
+var d = 2;
+
+var total2 = c * d
+console.log (total2)
+
+// 99/2 //
+var p = 99;
+var q = 2;
+
+var total3 = p/q
+console.log (total3)
+
+// 99%2 //
+var total4 = p%q
+console.log (total4)
